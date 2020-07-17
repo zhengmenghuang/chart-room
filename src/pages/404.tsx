9574f9cd0e493d32983cb617bb2406a440e8843f
llxhz <1481010383@qq.com>
2020-07-17 10:41:09 +0800

    push to master: 暂时告一段落

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,20 +2,21 @@ import { Button, Result } from 'antd';
 import React from 'react';
 import { history } from 'umi';
 
-export default class NoFoundPage extends React.Component {
-  render() {
-    return (
-      <Result
-        status="404"
-        title="404"
-        subTitle="对不起，您访问的页面不存在"
-        extra={
-          <Button type="primary" onClick={() => history.push('/')}>
-            {' '}
-            首页{' '}
-          </Button>
-        }
-      />
-    );
-  }
-}
+const NotFoundPage = () => {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="对不起，您访问的页面不存在"
+      extra={
+        <Button type="primary" onClick={() => history.push('/')}>
+          首页
+        </Button>
+      }
+    />
+  );
+};
+
+NotFoundPage.title = '404';
+
+export default NotFoundPage;
