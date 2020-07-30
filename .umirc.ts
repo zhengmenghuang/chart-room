c6820e2ac33449814249155e7555977aec14373c
llxhz <1481010383@qq.com>
2020-07-30 15:53:02 +0800

    push to master: 按需加载与转场动画冲突已修复

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  // dynamicImport: {
-  //   loading: '@/loading',
-  // },
+  dynamicImport: {
+    loading: '@/loading',
+  },
 });
