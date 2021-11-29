797257b49db45e245098979f7315cbf41ecbe5cc
llxhzm <1481010383@qq.com>
2021-11-29 15:29:08 +0800

    feat: 快速刷新

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  dynamicImport: {
-    loading: '@/loading',
-  },
-  antd: { mobile: false },
+  nodeModulesTransform: { type: 'none' }, // 设置 node_modules 目录下依赖文件的编译方式。
+  dynamicImport: { loading: '@/loading' }, // 按需加载
+  fastRefresh: {}, // 快速刷新
+  antd: { mobile: false }, // 避免 antd-mobile v5 编译报错
 });
