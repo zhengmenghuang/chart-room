0c34a0046ae6093d08b354b95d0f825dc83836a7
llxhzm <1481010383@qq.com>
2021-12-16 18:38:06 +0800

    feat: pdf预览

--- src/pages/tabs/tab3.tsx
+++ src/pages/tabs/tab3.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// @ts-ignore
+import Pdfh5 from 'pdfh5';
+import 'pdfh5/css/pdfh5.css';
+
+// https://github.com/gjTool/pdfh5
+const fileUrl = 'https://www.gjtool.cn/pdfh5/git.pdf';
 
 const IndexPage = () => {
-  return <div>tab3</div>;
+  useEffect(() => {
+    const pdfh5 = new Pdfh5('#demo', {
+      pdfurl: fileUrl,
+      lazy: true,
+    });
+
+    pdfh5.on('complete', (status: string, msg: string, time: string) => {
+      console.log(status, msg, time);
+    });
+  }, []);
+
+  return <div id="demo" />;
 };
 IndexPage.title = 'tab3';
 export default IndexPage;
